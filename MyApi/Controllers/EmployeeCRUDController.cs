33fe212f72cb19f512957aeeeeda7dfa5e84fe82
kansas martinez <kansasgears@hotmail.com>
2018-04-03 23:08:14 -0500

    Add project files.

---
+++ MyApi/Controllers/EmployeeCRUDController.cs
@@ -0,0 +1,105 @@
+﻿using MyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyApi.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        private ApiEntities db = new ApiEntities();
+
+        // GET: api/Employees  
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+
+
+        // PUT: api/Employees/5  
+
+        public HttpResponseMessage PutEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+
+            db.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST: api/Employees  
+
+        public HttpResponseMessage PostEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, employee);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = employee.EmployeeID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+        }
+
+        // DELETE: api/Employees/5  
+
+        public HttpResponseMessage DeleteEmployee(Employee employee)
+        {
+            Employee remove_employee = db.Employees.Find(employee.EmployeeID);
+            if (remove_employee == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Employees.Remove(remove_employee);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return db.Employees.Count(e => e.EmployeeID == id) > 0;
+        }
+    }
+}
